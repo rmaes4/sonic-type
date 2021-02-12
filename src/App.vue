22bcc0de49dbce0f4685c3cd47ba143d41850fef
Rob Maes <remilmaes@gmail.com>
2021-02-12 15:05:22 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+  </div>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style></style>
---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { RouteRecordRaw, createWebHistory, createRouter } from "vue-router";
+import Home from "../views/Home.vue";
+import NotFound from "../views/NotFound.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/:catchAll(.*)",
+    component: NotFound,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
